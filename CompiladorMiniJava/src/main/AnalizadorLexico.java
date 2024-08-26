ecb5e268517e73cc2a5b5401b5844ae72fc787ab
Goinza <gonzalo-am@hotmail.com>
2024-08-25 23:25:28 -0300

    AF casi completo y arreglo con la lectura de caracteres.
    Se arreglo un problema con la lectura de caracteres.
    Antes cada metodo leia un caracter al principio, ahora leen un caracter
    antes de llamar a otro metodo (y no leen cuando el metodo retorna un
    token).
    
    Se agregaron los metodos necesarios para los identificadores de
    puntuaciones, operadores y asignaciones.
    
    Faltan agregar metodos para procesar los comentarios, tanto los de una
    linea como los multi linea.

--- CompiladorMiniJava/src/main/AnalizadorLexico.java
+++ CompiladorMiniJava/src/main/AnalizadorLexico.java
@@ -18,6 +18,7 @@ public class AnalizadorLexico {
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
+		updateLastReadChar();
 	}
 	
 	public Token getNextToken() throws ExcepcionLexica {
@@ -76,49 +77,157 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e0() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (Character.isWhitespace(lastReadChar)) {
+			updateLastReadChar();
 			return e0();
 		}
 		if (Character.isUpperCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e1();
 		}
 		if (Character.isLowerCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e2();
 		}
 		if (Character.isDigit(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e3();
 		}
 		if (lastReadChar == '\'') {
 			updateLexema();
+			updateLastReadChar();
 			return e4();
 		}
 		if (lastReadChar == '"') {
 			updateLexema();
+			updateLastReadChar();
 			return e8();
 		}
-		
-		//Faltan puntuacion, operadores, asignacion
-		//Cada signo de puntuacion es un tipo de token distinto?
+		if (lastReadChar == '(') {
+			updateLexema();
+			updateLastReadChar();
+			return e12();
+		}
+		if (lastReadChar == ')') {
+			updateLexema();
+			updateLastReadChar();
+			return e13();
+		}
+		if (lastReadChar == '{') {
+			updateLexema();
+			updateLastReadChar();
+			return e14();
+		}
+		if (lastReadChar == '}') {
+			updateLexema();
+			updateLastReadChar();
+			return e15();
+		}
+		if (lastReadChar == ';') {
+			updateLexema();
+			updateLastReadChar();
+			return e16();
+		}
+		if (lastReadChar == ',') {
+			updateLexema();
+			updateLastReadChar();
+			return e17();
+		}
+		if (lastReadChar == '.') {
+			updateLexema();
+			updateLastReadChar();
+			return e18();
+		}
+		if (lastReadChar == ':') {
+			updateLexema();
+			updateLastReadChar();
+			return e19();
+		}
+		if (lastReadChar == '>') {
+			updateLexema();
+			updateLastReadChar();
+			return e20();
+		}
+		if (lastReadChar == '<') {
+			updateLexema();
+			updateLastReadChar();
+			return e22();	
+		}
+		if (lastReadChar == '!') {
+			updateLexema();
+			updateLastReadChar();
+			return e24();
+		}
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e26();
+		}
+		if (lastReadChar == '+') {
+			updateLexema();
+			updateLastReadChar();
+			return e28();
+		}
+		if (lastReadChar == '-') {
+			updateLexema();
+			updateLastReadChar();
+			return e30();
+		}
+		if (lastReadChar == '*') {
+			updateLexema();
+			updateLastReadChar();
+			return e32();
+		}
+		if (lastReadChar == '/') {
+			updateLexema();
+			updateLastReadChar();
+			return e33();
+		}
+		if (lastReadChar == '%') {
+			updateLexema();
+			updateLastReadChar();
+			return e34();
+		}
+		if (lastReadChar == '&') {
+			updateLexema();
+			updateLastReadChar();
+			return e35();
+		}
+		if (lastReadChar == '|') {
+			updateLexema();
+			updateLastReadChar();
+			return e37();
+		}
+		if (lastReadChar == SourceManager.END_OF_FILE) {
+			updateLexema();
+			try {
+				io.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+			return e39();
+		}
 		
 		throw new ExcepcionLexica(lexema, io.getLineNumber());		
 	}
 	
 	private Token e1() {
-		updateLastReadChar();
 		if (Character.isUpperCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e1();
 		}
 		if (Character.isLowerCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e1();
 		}
 		if (Character.isDigit(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e1();
 		}
 		
@@ -126,17 +235,19 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e2() {
-		updateLastReadChar();
 		if (Character.isUpperCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e2();
 		}
 		if (Character.isLowerCase(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e2();
 		}
 		if (Character.isDigit(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e2();
 		}
 		
@@ -144,9 +255,9 @@ public class AnalizadorLexico {
 	}	
 
 	private Token e3() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (Character.isDigit(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e3();
 		}
 		
@@ -158,9 +269,9 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e4() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (lastReadChar == '\\') {
 			updateLexema();
+			updateLastReadChar();
 			return e5();
 		}
 		if (lastReadChar == '\'') {
@@ -168,19 +279,20 @@ public class AnalizadorLexico {
 		}
 		
 		updateLexema();
+		updateLastReadChar();
 		return e6();
 	}
 	
 	private Token e5() throws ExcepcionLexica {
-		updateLastReadChar();
 		updateLexema();
+		updateLastReadChar();
 		return e6();
 	}
 	
 	private Token e6() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (lastReadChar == '\'') {
 			updateLexema();
+			updateLastReadChar();
 			return e7();
 		}
 		
@@ -192,9 +304,9 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e8() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (Character.isAlphabetic(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e9();
 		}
 		
@@ -202,17 +314,19 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e9() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (Character.isAlphabetic(lastReadChar)) {
 			updateLexema();
+			updateLastReadChar();
 			return e9();
 		}
 		if (lastReadChar == '"') {
 			updateLexema();
+			updateLastReadChar();
 			return e10();
 		}
 		if (lastReadChar == '\\') {
 			updateLexema();
+			updateLastReadChar();
 			return e11();
 		}
 		
@@ -224,13 +338,165 @@ public class AnalizadorLexico {
 	}
 	
 	private Token e11() throws ExcepcionLexica {
-		updateLastReadChar();
 		if (lastReadChar == '"') {
 			updateLexema();
+			updateLastReadChar();
 			return e9();
 		}
 		
 		throw new ExcepcionLexica(lexema, io.getLineNumber());
+	}
+	
+	private Token e12() {
+		return new Token("parentesisInicio", lexema, io.getLineNumber());
+	}
+	
+	private Token e13() {
+		return new Token("parentesisFin", lexema, io.getLineNumber());
+	}
+	
+	private Token e14() {
+		return new Token("llaveInicio", lexema, io.getLineNumber());
+	}
+	
+	private Token e15() {
+		return new Token("llaveFin", lexema, io.getLineNumber());
+	}
+	
+	private Token e16() {
+		return new Token("puntoComa", lexema, io.getLineNumber());
+	}
+	
+	private Token e17() {
+		return new Token("coma", lexema, io.getLineNumber());
+	}
+	
+	private Token e18() {
+		return new Token("punto", lexema, io.getLineNumber());
+	}
+	
+	private Token e19() {
+		return new Token("dosPuntos", lexema, io.getLineNumber());
+	}
+	
+	private Token e20() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e21();
+		}
+		return new Token("opMayor", lexema, io.getLineNumber());
+	}
+	
+	private Token e21() {
+		return new Token("opMayorIgual", lexema, io.getLineNumber());
+	}
+	
+	private Token e22() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e23();
+		}
+		return new Token("opMenor", lexema, io.getLineNumber());
+	}
+	
+	private Token e23() {
+		return new Token("opMenorIgual", lexema, io.getLineNumber());
+	}
+	
+	private Token e24() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e25();
+		}
+		return new Token("opNegacion", lexema, io.getLineNumber());
+	}
+	
+	private Token e25() {
+		return new Token("opDistinto", lexema, io.getLineNumber());
+	}
+	
+	private Token e26() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e27();
+		}
+		return new Token("asignacion", lexema, io.getLineNumber());
+	}
+	
+	private Token e27() {
+		return new Token("opIgual", lexema, io.getLineNumber());
+	}
+	
+	private Token e28() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e29();
+		}
+		return new Token("opSuma", lexema, io.getLineNumber());
+	}
+	
+	private Token e29() {
+		return new Token("asignacionSuma", lexema, io.getLineNumber());
+	}
+	
+	private Token e30() {
+		if (lastReadChar == '=') {
+			updateLexema();
+			updateLastReadChar();
+			return e31();
+		}
+		return new Token("opResta", lexema, io.getLineNumber());
+	}
+	
+	private Token e31() {
+		return new Token("asignacionResta", lexema, io.getLineNumber());
+	}
+	
+	private Token e32() {
+		return new Token("opMultiplicacion", lexema, io.getLineNumber());
+	}
+	
+	private Token e33() {
+		return new Token("opDivision", lexema, io.getLineNumber());
+	}
+	
+	private Token e34() {
+		return new Token("opModulo", lexema, io.getLineNumber());
+	}
+	
+	private Token e35() throws ExcepcionLexica {
+		if (lastReadChar == '&') {
+			updateLexema();
+			updateLastReadChar();
+			return e36();
+		}
+		throw new ExcepcionLexica(lexema, io.getLineNumber());
+	}
+	
+	private Token e36() {
+		return new Token("opAnd", lexema, io.getLineNumber());
+	}
+	
+	private Token e37() throws ExcepcionLexica {
+		if (lastReadChar == '|') {
+			updateLexema();
+			updateLastReadChar();
+			return e38();
+		}
+		throw new ExcepcionLexica(lexema, io.getLineNumber());
+	}
+	
+	private Token e38() {
+		return new Token("opOr", lexema, io.getLineNumber());
+	}
+	
+	private Token e39() {
+		return new Token("EOF", lexema, io.getLineNumber());
 	}
 	
 }
